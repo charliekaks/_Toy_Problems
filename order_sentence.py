eac4e846e52493ef1888ef634349bf54ab05606e
charliekaks <kaksunchained@gmail.com>
2018-01-26 15:16:36 +0300

    Skeleton code and the instructions for solving the toy problem

---
+++ order_sentence.py
@@ -0,0 +1,10 @@
+'''
+Your task is to sort a given string. Each word in the String will contain a single number.
+This number is the position the word should have in the result.
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+'''
+
+
+def order(sentence):
+    
+    return
